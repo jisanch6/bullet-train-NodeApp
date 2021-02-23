d3a6c350789d428b270559cc36b447c712f63d56
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-23 13:53:43 -0600

    added basic CRUD operations to Departures

--- controllers/departureController.js
+++ controllers/departureController.js
@@ -4,24 +4,94 @@ exports.getAllDepartures = async (req, res) => {
   try {
     const departures = await Departure.find();
 
-    res.status(204).json({
-      status: 'SUCCESS',
+    return res.status(200).json({
+      status: 'success',
+      results: departures.length,
       data: {
         departures,
       },
     });
-  } catch (error) {
-    return res.status(400).json({
-      status: 'FAIL',
-      message: error.message,
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      message: err,
     });
   }
 };
 
-exports.createDeparture = async (req, res) => {};
+exports.createDeparture = async (req, res) => {
+  try {
+    const newDeparture = await Departure.create(req.body);
 
-exports.getDeparture = async (req, res) => {};
+    res.status(201).json({
+      status: 'success',
+      data: {
+        departure: newDeparture,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
 
-exports.deleteDeparture = async (req, res) => {};
+exports.getDeparture = async (req, res) => {
+  try {
+    const departure = await Departure.findById(req.params.id);
 
-exports.updateDeparture = async (req, res) => {};
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        departure,
+      },
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.updateDeparture = async (req, res) => {
+  try {
+    const departure = await Departure.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        departure,
+      },
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.deleteDeparture = async (req, res) => {
+  try {
+    await Departure.findByIdAndDelete(req.params.id);
+
+    return res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
