86629265829b4cf76552fed6c60911a480c45039
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-23 11:53:46 -0600

    established connection to remote db on mongo atlas

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -6,6 +7,23 @@ const app = require('./app');
 
 // console.log(process.env);
 // console.log(app.get('env'));
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(con.connection);
+    console.log('DB connection established!');
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
