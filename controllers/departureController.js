b9b60899002615d6677c616f01119d3319fcfaba
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-23 19:38:13 -0600

    refactored sort,limit,paginate,filter to own class

--- controllers/departureController.js
+++ controllers/departureController.js
@@ -1,9 +1,14 @@
 const Departure = require('../models/departureModel');
 
-exports.getAllDepartures = async (req, res) => {
-  try {
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
     // FILTERING
-    const queryObj = { ...req.query };
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
     excludedFields.forEach((el) => delete queryObj[el]);
@@ -14,37 +19,55 @@ exports.getAllDepartures = async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     // console.log(JSON.parse(queryStr));
 
-    let query = Departure.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
+    // let query = Departure.find(JSON.parse(queryStr));
+    return this;
+  }
 
+  sort() {
     //SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      this.query = this.query.sort('-createdAt');
     }
 
+    return this;
+  }
+
+  limitFields() {
     //FIELD LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
     } else {
-      query = query.select('-__v');
+      this.query = this.query.select('-__v');
     }
+    return this;
+  }
 
+  paginate() {
     //PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 10;
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 10;
     const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numDepartures = await Departure.countDocuments();
-      if (skip >= numDepartures) throw new Error('this page does not exist');
-    }
+    this.query = this.query.skip(skip).limit(limit);
 
+    return this;
+  }
+}
+
+exports.getAllDepartures = async (req, res) => {
+  try {
     //EXECUTE QUERY
-    const departures = await query;
+    const features = new APIFeatures(Departure.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const departures = await features.query;
 
     res.status(200).json({
       status: 'success',
