0f72d2745ea1a9ab515dd8afd87c940d6bb8c9d8
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-24 13:55:25 -0600

    adding error handling for specific enviroments

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, errors that are trusted: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    //Unknown programming or other errors: do not leak information
+  } else {
+    //logging error
+    console.log('ERROR', err);
+
+    //sending generic err message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
