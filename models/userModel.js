580a67a80464a31086144dcd2bed5b182ac74d0f
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-24 15:54:32 -0600

    created user model from schema

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'Please tell us your name!'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide us your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+  photo: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
