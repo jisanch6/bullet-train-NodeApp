2135967ffaa45466b8d8bb8694af088df000f1b9
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-24 17:36:52 -0600

    instance method to verify password when logging in

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -48,6 +49,14 @@ userSchema.pre('save', async function (next) {
   //delete passwordConfirm field
   this.passwordConfirm = undefined;
 });
+
+//Instance method to return true if passwords are the same
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
