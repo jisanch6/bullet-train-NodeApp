972a96d4331f1efa86467f05395c5e31ad82f372
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-24 19:58:29 -0600

    added protections and restrictions to routes

--- routes/departureRoutes.js
+++ routes/departureRoutes.js
@@ -7,11 +7,19 @@ const router = express.Router();
 router
   .route('/')
   .get(departureController.getAllDepartures)
-  .post(departureController.createDeparture);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'train-operator'),
+    departureController.createDeparture
+  );
 router
   .route('/:id')
   .get(departureController.getDeparture)
-  .patch(departureController.updateDeparture)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'train-operator'),
+    departureController.updateDeparture
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'train-operator'),
