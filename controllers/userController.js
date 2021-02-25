4ffce0c6d38c35812d5b022ca248377f70dcb331
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-25 17:17:23 -0600

    worked around to getOne using current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // Create error if user attempts to pass password data
   if (req.body.password || req.body.passwordConfirm) {
