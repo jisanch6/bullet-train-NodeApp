7b5e4948d404a311b6d987b7ba481dc04c05a171
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-24 19:15:52 -0600

    added restriction of roles user model expects

--- models/userModel.js
+++ models/userModel.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
   photo: String,
   role: {
     type: String,
+    enum: ['user', 'train-operator', 'admin'],
     default: 'user',
   },
   passwordChangedAt: Date,
