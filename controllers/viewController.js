364c0b265e059fad538689061093e3df762b4d3c
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-26 17:37:07 -0600

    updated title

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -19,7 +19,7 @@ exports.getDeparture = catchAsync(async (req, res) => {
   );
 
   res.status(200).render('departure', {
-    title: `${departure.name}`,
+    title: `Departing to ${departure.name}`,
     departure,
   });
 });
