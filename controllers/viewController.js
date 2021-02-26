c0543405f542764c16400737e64296a513343852
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-26 14:40:23 -0600

    rendering cards with departures information

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+const Departure = require('../models/departureModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  const departures = await Departure.find();
+
   res.status(200).render('overview', {
     title: 'All Departures',
+    departures,
   });
-};
+});
 
 exports.getDeparture = (req, res) => {
   res.status(200).render('departure', {
