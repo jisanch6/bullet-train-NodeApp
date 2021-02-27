7764d60b01f2228b8857f1f52f559c38c558b16b
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-27 16:32:59 -0600

    logged user on webpage using API

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+require('@babel/polyfill');
+const axios = require('axios');
+
+const loginForm = document.querySelector('.form--login');
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log(email, password);
+    login(email, password);
+  });
+}
+
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    alert(err.response.data.message);
+  }
+};
