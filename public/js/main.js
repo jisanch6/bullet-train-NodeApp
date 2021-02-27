a78b2009479a889b6deec271addea30755ac383d
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-27 17:23:22 -0600

    implemented user sign up

--- public/js/main.js
+++ public/js/main.js
@@ -15,14 +15,25 @@ const showAlert = (type, msg) => {
 };
 
 const loginForm = document.querySelector('.form--login');
+const signupForm = document.querySelector('.form--signup');
 
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    console.log(email, password);
     login(email, password);
+  });
+}
+
+if (signupForm) {
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
   });
 }
 
@@ -38,6 +49,29 @@ const login = async (email, password) => {
     });
     if (res.data.status === 'success') {
       showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
+const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm,
+      },
+    });
+    if (res.data.status === 'success') {
+      showAlert('success', 'Signed up successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
