48449211222ff49a4c40f9f8deb9aa0b3000c4af
Jorge Sanchez <jsanchez.00@outlook.com>
2021-02-27 17:08:44 -0600

    bundle main.js with alerts

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9002,6 +9002,18 @@ try {
 require('@babel/polyfill');
 const axios = require('axios');
 
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
+
 const loginForm = document.querySelector('.form--login');
 
 if (loginForm) {
@@ -9025,13 +9037,13 @@ const login = async (email, password) => {
       },
     });
     if (res.data.status === 'success') {
-      alert('Logged in successfully!');
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
 
