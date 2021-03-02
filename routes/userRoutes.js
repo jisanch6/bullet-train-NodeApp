f0bb8c1b26e56e1cf1eefe2b08ea04b5dd507d75
Jorge Sanchez <jsanchez.00@outlook.com>
2021-03-02 16:06:09 -0600

    added user image upload functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
@@ -15,7 +16,11 @@ router.use(authController.protect);
 router.patch('/updateMyPassword', authController.updatePassword);
 
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.updateMe
+);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
