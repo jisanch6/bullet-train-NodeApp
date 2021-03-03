7d379d2346780a8256b54856bfd614c13fd89367
Jorge Sanchez <jsanchez.00@outlook.com>
2021-03-02 20:46:48 -0600

    adapting email to work in diff envs & situations

--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,54 @@
+const path = require('path');
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { htmlToText } = require('html-to-text');
+// new Email(user, url).sendWelcome();
 
-const sendEmail = async (options) => {
-  // CREATE a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // DEFINE the email options
-  const mailOptions = {
-    from: 'Jorge Sanchez <jorge.i.sanchez.2018@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  // SEND the email
-  await transporter.sendMail(mailOptions);
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Jorge Sanchez <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(
+      path.join(__dirname, `views/emails/${template}.pug`),
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    this.send('Welcome', 'Welcome to the Bullet-Train family! ');
+  }
 };
-
-module.exports = sendEmail;
