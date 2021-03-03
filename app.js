95c6c596778fe132f59345c7b23ee9854c55abb1
Jorge Sanchez <jsanchez.00@outlook.com>
2021-03-03 13:14:21 -0600

    commented out console.logs & updated template

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ app.use(
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  if (res.locals.token) console.log(res.locals.token);
-  console.log(req.originalUrl);
-  console.log('Hello from the global middleware');
+  // if (res.locals.token) console.log(res.locals.token);
+  // console.log(req.originalUrl);
+  // console.log('Hello from the global middleware');
   next();
 });
 
