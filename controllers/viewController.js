706e288917d3ae25a019cf1c8379295638ff7fe8
Jorge Sanchez <jsanchez.00@outlook.com>
2021-03-09 12:35:56 -0600

    removed uneeded code

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,37 +5,6 @@ const Departure = require('../models/departureModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-// const signToken = (id) =>
-//   jwt.sign({ id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRES_IN,
-//   });
-
-// const createSendToken = (user, statusCode, res) => {
-//   const token = signToken(user._id);
-
-//   const cookieOptions = {
-//     expires: new Date(
-//       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-//     ),
-//     httpOnly: true,
-//   };
-
-//   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-//   res.cookie('jwt', token);
-
-//   //removes password from output of creating new doc
-//   user.password = undefined;
-
-//   res.status(statusCode).json({
-//     status: 'success',
-//     token,
-//     data: {
-//       user,
-//     },
-//   });
-// };
-
 exports.getOverview = catchAsync(async (req, res, next) => {
   const departures = await Departure.find();
 
@@ -85,31 +54,8 @@ exports.getAccount = (req, res) =>
     title: 'Your account',
   });
 
-exports.getResetPasswordForm = catchAsync(async (req, res, next) => {
-  // // GET user based on the token
-  // const hashedToken = crypto
-  //   .createHash('sha256')
-  //   .update(req.params.token)
-  //   .digest('hex');
-
-  // const user = await User.findOne({
-  //   passwordResetToken: hashedToken,
-  //   passwordResetExpires: { $gt: Date.now() },
-  // });
-  // // if toke has not EXPIRED, & user exists, set the new password
-  // if (!user) {
-  //   return next(new AppError('Token is invalid or has expired.', 400));
-  // }
-  // user.password = req.body.password;
-  // user.passwordConfirm = req.body.passwordConfirm;
-  // user.passwordResetToken = undefined;
-  // user.passwordResetExpires = undefined;
-  // await user.save();
-
-  // // Log the user in, send JWT
-  // createSendToken(user, 200, res);
-
+exports.getResetPasswordForm = (req, res) => {
   res.status(200).render('resetPassword', {
     title: 'Reset your password',
   });
-});
+};
