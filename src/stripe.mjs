10c847f4282165efb2d27224844c57b69a038241
Jorge Sanchez <jsanchez.00@outlook.com>
2021-03-12 18:36:25 -0600

    attempting online payment using stripe

--- src/stripe.mjs
+++ src/stripe.mjs
@@ -1,12 +1,12 @@
 /* eslint-disable */
-import axios from 'axios';
-const stripe = Stripe(
-  pk_test_51IJ9XaJhWumwQBaCJH6Gdzl98ZIQRKkHFaRdDo9cVb58YY6Rb1hswTOCAGZ2NLmn9ml9hZMFGtKQhvdObYEPqLKn00tuoT388a
-);
+// import axios from 'axios';
+// const stripe = Stripe(
+//   pk_test_51IJ9XaJhWumwQBaCJH6Gdzl98ZIQRKkHFaRdDo9cVb58YY6Rb1hswTOCAGZ2NLmn9ml9hZMFGtKQhvdObYEPqLKn00tuoT388a
+// );
 
-export const bookDeparture = async (departureId) => {
-  //Checkout session from departure endpoint
-  const session = await axios(`/api/v1/bookings/checkout-session/:departureId`);
-  console.log(session);
-  //use stripe object to make checkout form & charge credit card
-};
+// export const bookDeparture = async (departureId) => {
+//   //Checkout session from departure endpoint
+//   const session = await axios(`/api/v1/bookings/checkout-session/:departureId`);
+//   console.log(session);
+//   //use stripe object to make checkout form & charge credit card
+// };
