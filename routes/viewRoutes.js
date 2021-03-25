461101ad7e4d0ef0b0fd115ab3437299793b33f9
Jorge Sanchez <jsanchez.00@outlook.com>
2021-03-25 11:37:06 -0500

    added protected route to user viewing own bookings

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
-// const userRouter = require('./userRoutes');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-// router
-//   .route('/resetPassword/:resetToken/users', userRouter)
-//   .patch(authController.resetPassword);
-
-// router.get('/users/:token/resetPassword', viewController.getResetPasswordForm);
-
 router.get('/account', authController.protect, viewController.getAccount);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewController.getOverview
+);
 
 router.use(authController.isLoggedIn);
 
-router.get('/', viewController.getOverview);
 router.get('/forgotPassword', viewController.getForgotPasswordForm);
 router.get('/departure/:slug', viewController.getDeparture);
 router.get('/login', viewController.getLoginForm);
 router.get('/signup', viewController.getSignupForm);
+router.get(
+  '/my-bookings',
+  authController.protect,
+  viewController.getMyBookings
+);
 
 module.exports = router;
