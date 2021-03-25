432c10f9750c95cf60f8c3cd8486e6eff5da654a
Jorge Sanchez <jsanchez.00@outlook.com>
2021-03-25 10:15:34 -0500

    created booking model

---
+++ models/bookingModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  departure: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Departure',
+    required: [true, 'Booking must belong to a Tour!'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a user'],
+  },
+  price: {
+    type: Number,
+    require: [true, 'Booking a departure must have a price'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({ path: 'departure', select: 'name' });
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
